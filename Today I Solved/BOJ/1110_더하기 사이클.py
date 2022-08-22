9d969f15ffb295418bcc22dc1044add1d6e57ff1
hjs721 <dse04145@gmail.com>
2022-08-23 00:22:11 +0900

    0822 coding practice

---
+++ Today I Solved/BOJ/1110_더하기 사이클.py
@@ -0,0 +1,27 @@
+N = input()
+num = N
+cnt = 0
+while True:
+    if len(num) == 1:
+        num = '0' + num
+    sum_ = str(int(num[0]) + int(num[1]))
+    num = num[1] + sum_[-1]
+    cnt += 1
+    if N == num:
+        break
+print(cnt)
+# 시간초과...
+
+
+N = int(input())
+num = N
+cnt = 0
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = b * 10 + c
+    cnt += 1
+    if num == N:
+        break
+print(cnt)
